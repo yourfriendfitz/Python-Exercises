48b65683a9abeb9e1cff46e06158fffaf6194b27
JC Tovar <jctovar.create@gmail.com>
2019-06-25 15:13:14 -0500

    Add files via upload

---
+++ introduction.py
@@ -0,0 +1,6 @@
+#String Interopolation
+
+firstName = input("Hey! Type your first name right after this colon: ")
+lastName= input("Now hit me with your last name! ")
+
+print(f"Yo! Very Nice to meet you {firstName} {lastName} ")
