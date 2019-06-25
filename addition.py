564b9566baf9412b8b52a54127a738e529b56f71
Juan Tovar <jct_speakers@Juans-MacBook-Pro-2.local>
2019-06-25 15:33:05 -0500

    the rest of the fundamental lessons

---
+++ addition.py
@@ -0,0 +1,8 @@
+#addition in python
+
+firstNumber = input("Give me an integer! ")
+secondNumber = input("Alright, now another integer! ")
+
+total = int(firstNumber) + int(secondNumber)
+
+print(f"The Sum of your intergers is {total}")
