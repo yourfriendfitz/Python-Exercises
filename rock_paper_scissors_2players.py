538edd6f99c44b3f602dccb5624bcba18666a043
JC Tovar <jctovar.create@gmail.com>
2019-07-05 21:04:53 -0500

    Rock/Paper/Scissors Game with 2 Players
    Game works but players can see each others selections so Player 2 would always win. Could have put some lines in-between so player 2 couldn't see what player one had played.

---
+++ rock_paper_scissors_2players.py
@@ -0,0 +1,38 @@
+def RPS():
+    print("Rock")
+    print("Paper")
+    print("Scissors")
+RPS()
+
+player1 = input("Player 1 Enter your Selection: ")
+player2 = input("Player 2 Enter your Selection: ")
+   
+player1 = player1.capitalize()
+player2 = player2.capitalize()
+
+if player1 == player2:
+    print("It's a Tie!")
+elif player1 == "Rock":
+    if player2 == "Paper":
+        print("Player 2 Wins")
+    elif player2 == "Scissors":
+        print("Player 2 Wins")
+    else:
+        print("Player 2 made an invalid entry.")
+elif player1 == "Paper":
+    if player2 == "Scissors":
+        print("Player 1 Wins")
+    elif player2 == "Rock":
+        print("Player 2 Wins")
+    else:
+        print("Player 2 made an invalid entry.")
+elif player1 == "Scissors":
+    if player2 == "Paper":
+        print("Player 1 Wins")
+    elif player2 == "Rock":
+        print("Player 2 Wins")
+    else:
+        print("Player 2 made an invalid entry.")
+else:
+    print("Your selection was not valid.")
+
