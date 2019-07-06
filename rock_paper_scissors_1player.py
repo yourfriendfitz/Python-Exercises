10dff4114897477eaf9efdd02c4dc01eb88691c2
J. Fitzgerald <48025555+yourfriendfitz@users.noreply.github.com>
2019-07-06 11:48:45 -0500

    Update rock_paper_scissors_1player.py
    changed the .capitalize on the player1 and player2 strings to .lower to avoid errors if input was rOck or SCissors for example
    changed the if and elifs to lower case to match the .lower method on the strings
    fixed player1 == "rock" so that if player2 = "scissors" it displays "Player 1 Wins" instead of "Player 2 Wins"

--- rock_paper_scissors_1player.py
+++ rock_paper_scissors_1player.py
@@ -9,38 +9,38 @@ RPS()
 random_draw = random.randint(1,3)
 
 if random_draw == 1:
-    player2 = "Rock"
+    player2 = "rock"
 elif random_draw == 2:
-    player2 = "Paper"
+    player2 = "paper"
 elif random_draw == 3:
-    player2 = "Scissors"
+    player2 = "scissors"
 
 player1 = input("Player 1 Enter your Selection: ")
 print(f"Player 2 Drew {player2}")
    
-player1 = player1.capitalize()
-player2 = player2.capitalize()
+player1 = player1.lower()
+player2 = player2.lower()
 
 if player1 == player2:
     print("It's a Tie!")
-elif player1 == "Rock":
-    if player2 == "Paper":
+elif player1 == "rock":
+    if player2 == "paper":
         print("Player 2 Wins")
-    elif player2 == "Scissors":
+    elif player2 == "scissors":
+        print("Player 1 Wins")
+    else:
+        print("Player 2 made an invalid entry.")
+elif player1 == "paper":
+    if player2 == "scissors":
+        print("Player 1 Wins")
+    elif player2 == "rock":
         print("Player 2 Wins")
     else:
         print("Player 2 made an invalid entry.")
-elif player1 == "Paper":
-    if player2 == "Scissors":
+elif player1 == "scissors":
+    if player2 == "paper":
         print("Player 1 Wins")
-    elif player2 == "Rock":
-        print("Player 2 Wins")
-    else:
-        print("Player 2 made an invalid entry.")
-elif player1 == "Scissors":
-    if player2 == "Paper":
-        print("Player 1 Wins")
-    elif player2 == "Rock":
+    elif player2 == "rock":
         print("Player 2 Wins")
     else:
         print("Player 2 made an invalid entry.")
